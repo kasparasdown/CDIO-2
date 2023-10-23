8ff7cb1ba7be847f338df8f0bdec6a8866616625
Victor Mazanti <victormazanti@hotmail.com>
2023-10-23 14:14:54 +0200

    removed cluster in player class

--- Player.java
+++ Player.java
@@ -1,10 +1,4 @@
 public class Player{
     boolean isTurn;
     String name;
-
-    public static void main(String[] args){
-        if(isTurn = true){
-            player.rollDice
-        }
-    }
 }
