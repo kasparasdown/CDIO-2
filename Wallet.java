56329f7c427099c6ea02d00e188b6cdf2e4021a7
emilyoxdk06 <emily_c@live.dk>
2023-10-25 16:44:43 +0200

    wallet is done

--- Wallet.java
+++ Wallet.java
@@ -1,26 +1,38 @@
-public class Wallet {
-    
-    int coinBalance = 1000;
+public class Wallet{
+    private int coinBalance;
 
-    public Wallet(int coinBalance){
-        this.coinBalance = coinBalance;
+    public Wallet(int initialBalance) {
+        this.coinBalance = initialBalance;
     }
 
-    public int getCoinBalance(){
+    public int getCoinBalance() {
         return coinBalance;
     }
 
-    public void addCoins(int amount){
-        this.coinBalance =+ amount;
-        if(amount > 0){
-           System.out.println(amount + " coins have been added to your wallet"); 
+    public void addCoins(int amount) {
+        if (amount > 0) {
+            coinBalance += amount;
+            System.out.println(amount + " coins added to the wallet. Your balance is " + coinBalance);
+        } else {
+            System.out.println("Invalid amount.");
         }
-        else if(amount < 0){
-           System.out.println(amount + " coins have been subtracted to your wallet"); 
-        }
-        else{
-            System.out.println("Invalid amount");
-        }
+    }
 
+    public void subtractCoins(int amount) {
+        if (amount > 0 && amount <= coinBalance) {
+            coinBalance -= amount;
+            System.out.println(amount + " coins subtracted from the wallet. Your balance is " + coinBalance);
+        } else if (amount > 0 && amount >= coinBalance) {
+            coinBalance = 0;
+            System.out.println("You`re broke.. Your balance is " + coinBalance);
+        } else {
+            System.out.println("Invalid amount.");
+        }
+    }
+    public static void main(String[] args){
+        Wallet p1Wal = new Wallet(1000);
+
+        p1Wal.addCoins(500);
+        p1Wal.subtractCoins(4000);
     }
 }
